88a4219573c31c1d097535e97891dd69049f4722
daniprimo <daniellopes0208@hotmail.com>
2023-07-23 14:25:26 -0300

    subindo alteracao para vercel

--- src/pages/ListarViagens/index.jsx
+++ src/pages/ListarViagens/index.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { bucsarViagem } from "../../service";
 import ListaViagemPassadas from "../../components/ListarViagens";
 
