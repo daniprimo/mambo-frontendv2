41035c204630fed8b1f9f7f56ea822c447bddb35
primodev <daniellopes0208@hotmail.com>
2023-08-04 14:38:55 -0300

    Scroll adicionado

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,12 +4,318 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <Link to="/programacao">
-      <div className="flex items-center p-10">
-        <h1 className="text-2xl">Take me to About Page</h1>
-        <button class Name="bg-blue-500 px-4 text-white ml-4 rounded-full">
-          Programacao
-        </button>
+
+
+      <div className="grid grid-cols-2 gap-2">
+        <div className="h-60 max-h-full hover:max-h-screen overflow-y-auto h-32">
+          <h1>Programado</h1>
+
+          <div>
+            <ul class="divide-y divide-gray-100">
+              <>
+                <li
+                  class="flex justify-between gap-x-6
+           py-5 ease-linear transform hover:scale-105
+            transition duration-500 bg-red-900 px-6 py-2 m-5 inline rounded-t-2xl"
+                >
+                  <div class="flex gap-x-4">
+                    <div class="min-w-0 flex-auto">
+                      <p class="text-sm font-semibold leading-6 text-white">
+                        FDC1A50
+                      </p>
+                      <p class="mt-1 truncate text-xs leading-5 text-white">
+                        Motorista Adriano E
+                      </p>
+                      <div class="mt-1 flex items-center gap-x-1.5">
+                        <div class="flex-none rounded-full bg-blue-500/20 p-1">
+                          <div class="h-1.5 w-1.5 rounded-full bg-blue-500"></div>
+                        </div>
+                        <p class="text-xs leading-5 text-white">Programado</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="hidden sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Origem
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">CD MAMBO</p>
+                  </div>
+
+
+                  <div class="sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Destino
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">M2 - Leopoldina</p>
+
+                  </div>
+                </li>
+              </>
+            </ul>
+          </div>
+
+          <div>
+            <ul class="divide-y divide-gray-100">
+              <>
+                <li
+                  class="flex justify-between gap-x-6
+           py-5 ease-linear transform hover:scale-105
+            transition duration-500 bg-red-900 px-6 py-2 m-5 inline rounded-t-2xl"
+                >
+                  <div class="flex gap-x-4">
+                    <div class="min-w-0 flex-auto">
+                      <p class="text-sm font-semibold leading-6 text-white">
+                        FDC1A50
+                      </p>
+                      <p class="mt-1 truncate text-xs leading-5 text-white">
+                        Motorista Adriano E
+                      </p>
+                      <div class="mt-1 flex items-center gap-x-1.5">
+                        <div class="flex-none rounded-full bg-blue-500/20 p-1">
+                          <div class="h-1.5 w-1.5 rounded-full bg-blue-500"></div>
+                        </div>
+                        <p class="text-xs leading-5 text-white">Programado</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="hidden sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Origem
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">CD MAMBO</p>
+                  </div>
+
+
+                  <div class="sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Destino
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">M2 - Leopoldina</p>
+
+                  </div>
+                </li>
+              </>
+            </ul>
+          </div>
+          <div>
+            <ul class="divide-y divide-gray-100">
+              <>
+                <li
+                  class="flex justify-between gap-x-6
+           py-5 ease-linear transform hover:scale-105
+            transition duration-500 bg-red-900 px-6 py-2 m-5 inline rounded-t-2xl"
+                >
+                  <div class="flex gap-x-4">
+                    <div class="min-w-0 flex-auto">
+                      <p class="text-sm font-semibold leading-6 text-white">
+                        FDC1A50
+                      </p>
+                      <p class="mt-1 truncate text-xs leading-5 text-white">
+                        Motorista Adriano E
+                      </p>
+                      <div class="mt-1 flex items-center gap-x-1.5">
+                        <div class="flex-none rounded-full bg-blue-500/20 p-1">
+                          <div class="h-1.5 w-1.5 rounded-full bg-blue-500"></div>
+                        </div>
+                        <p class="text-xs leading-5 text-white">Programado</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="hidden sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Origem
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">CD MAMBO</p>
+                  </div>
+
+
+                  <div class="sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Destino
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">M2 - Leopoldina</p>
+
+                  </div>
+                </li>
+              </>
+            </ul>
+          </div>
+
+
+
+
+        </div>
+        <div className="h-60 max-h-full hover:max-h-screen overflow-y-auto h-32">
+          <h1>Em Crregamento</h1>
+
+          
+          <div>
+            <ul class="divide-y divide-gray-100">
+              <>
+                <li
+                  class="flex justify-between gap-x-6
+           py-5 ease-linear transform hover:scale-105
+            transition duration-500 bg-red-900 px-6 py-2 m-5 inline rounded-t-2xl"
+                >
+                  <div class="flex gap-x-4">
+                    <div class="min-w-0 flex-auto">
+                      <p class="text-sm font-semibold leading-6 text-white">
+                        FDC1A50
+                      </p>
+                      <p class="mt-1 truncate text-xs leading-5 text-white">
+                        Motorista Adriano E
+                      </p>
+                      <div class="mt-1 flex items-center gap-x-1.5">
+                        <div class="flex-none rounded-full bg-blue-500/20 p-1">
+                          <div class="h-1.5 w-1.5 rounded-full bg-blue-500"></div>
+                        </div>
+                        <p class="text-xs leading-5 text-white">Em Carregamento</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="hidden sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Origem
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">CD MAMBO</p>
+                  </div>
+
+
+                  <div class="sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Destino
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">M2 - Leopoldina</p>
+
+                  </div>
+                </li>
+              </>
+            </ul>
+          </div>
+
+
+
+        </div>
+        <div className="h-48 max-h-full hover:max-h-screen">
+          <h1>Carregado</h1>
+
+
+          <div>
+            <ul class="divide-y divide-gray-100">
+              <>
+                <li
+                  class="flex justify-between gap-x-6
+           py-5 ease-linear transform hover:scale-105
+            transition duration-500 bg-red-900 px-6 py-2 m-5 inline rounded-t-2xl"
+                >
+                  <div class="flex gap-x-4">
+                    <div class="min-w-0 flex-auto">
+                      <p class="text-sm font-semibold leading-6 text-white">
+                        FDC1A50
+                      </p>
+                      <p class="mt-1 truncate text-xs leading-5 text-white">
+                        Motorista Adriano E
+                      </p>
+                      <div class="mt-1 flex items-center gap-x-1.5">
+                        <div class="flex-none rounded-full bg-blue-500/20 p-1">
+                          <div class="h-1.5 w-1.5 rounded-full bg-blue-500"></div>
+                        </div>
+                        <p class="text-xs leading-5 text-white">Carregado</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="hidden sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Origem
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">CD MAMBO</p>
+                  </div>
+
+
+                  <div class="sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Destino
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">M2 - Leopoldina</p>
+
+                  </div>
+                </li>
+              </>
+            </ul>
+          </div>
+
+
+        </div>
+        <div className="h-48 max-h-full hover:max-h-screen overflow-y-auto h-32" >
+          <h1>Em Transito</h1>
+
+            <div>
+            <ul class="divide-y divide-gray-100">
+              <>
+                <li
+                  class="flex justify-between gap-x-6
+           py-5 ease-linear transform hover:scale-105
+            transition duration-500 bg-red-900 px-6 py-2 m-5 inline rounded-t-2xl"
+                >
+                  <div class="flex gap-x-4">
+                    <div class="min-w-0 flex-auto">
+                      <p class="text-sm font-semibold leading-6 text-white">
+                        FDC1A50
+                      </p>
+                      <p class="mt-1 truncate text-xs leading-5 text-white">
+                        Motorista Adriano E
+                      </p>
+                      <div class="mt-1 flex items-center gap-x-1.5">
+                        <div class="flex-none rounded-full bg-blue-500/20 p-1">
+                          <div class="h-1.5 w-1.5 rounded-full bg-blue-500"></div>
+                        </div>
+                        <p class="text-xs leading-5 text-white">Programado</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="hidden sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Origem
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">CD MAMBO</p>
+                  </div>
+
+
+                  <div class="sm:flex sm:flex-col">
+                    <p class="text-sm font-semibold leading-6 text-white">
+                      Destino
+                    </p>
+
+                    <p class="text-sm leading-6 text-white">M2 - Leopoldina</p>
+
+                  </div>
+                </li>
+              </>
+            </ul>
+          </div>
+
+
+        </div>
+
       </div>
+
     </Link>
   );
 }
