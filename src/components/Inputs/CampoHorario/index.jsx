0c27dd1c8f965e55ffb983943373f6580533b896
primodev <daniellopes0208@hotmail.com>
2023-08-09 17:55:49 -0300

    Voltando as excluidas

---
+++ src/components/Inputs/CampoHorario/index.jsx
@@ -0,0 +1,233 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { chegadaCD, 
+  chegadaLoja,
+   fimCarregamento,
+    saidaLoja } from "../../../service";
+
+
+export function InputConsulta(props) {
+
+  const mudarTelaPara = useNavigate();
+
+  const acaoInicioCarregamento = () => {
+    fimCarregamento(props.id)
+    mudarTelaPara(0)
+  }
+  const acaoFimCarregamento = () => {
+    fimCarregamento(props.id)
+    mudarTelaPara(0)
+  }
+  const acaoCahegadaLoja = () => {
+    chegadaLoja(props.id)
+    mudarTelaPara(0)
+  }
+  
+  const acaoSaidaLoja = () => {
+    saidaLoja(props.id)
+    mudarTelaPara(0)
+  }
+    
+  const acaoChegadaCD = () => {
+    chegadaCD(props.id)
+    mudarTelaPara(0)
+  }
+
+  return (
+    <div className="grid grid-cols-2 inline-block">
+      <div className="grid grid-cols-3 mt-3">
+
+      <div>
+          <label
+            for="first-name"
+            class="block text-sm font-medium leading-6 text-gray-900"
+          >
+            {"Programado"}
+          </label>
+          <div class="grid mt-2">
+            <input
+              class="block
+         w-20 rounded-md border-0 
+        py-1.5 text-gray-900 shadow-sm
+         ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+         focus:ring-2 focus:ring-inset focus:ring-indigo-600
+          sm:text-sm sm:leading-6 text-center"
+              value={props.programado}
+            />
+          </div>
+          <button
+            type="button"
+            className="rounded-md bg-indigo-600 
+           text-sm font-semibold text-white 
+           shadow-sm hover:bg-indigo-500
+            focus-visible:outline focus-visible:outline-2
+             focus-visible:outline-offset-2
+              focus-visible:outline-indigo-600"
+
+          >consolidar</button>
+
+        </div>
+
+
+        <div>
+          <label
+            for="first-name"
+            class="block text-sm font-medium leading-6 text-gray-900"
+          >
+            {"Carregamento Iniciado"}
+          </label>
+          <div class="grid mt-2">
+            <input
+              class="block
+         w-20 rounded-md border-0 
+        py-1.5 text-gray-900 shadow-sm
+         ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+         focus:ring-2 focus:ring-inset focus:ring-indigo-600
+          sm:text-sm sm:leading-6 text-center"
+              value={props.iniciarCarregamento}
+            />
+          </div>
+          <button
+            type="button"
+            className="rounded-md bg-indigo-600 
+           text-sm font-semibold text-white 
+           shadow-sm hover:bg-indigo-500
+            focus-visible:outline focus-visible:outline-2
+             focus-visible:outline-offset-2
+              focus-visible:outline-indigo-600"
+            onClick={() => acaoInicioCarregamento()}
+          >consolidar</button>
+
+        </div>
+
+        <div>
+          <label
+            for="first-name"
+            class="block text-sm font-medium leading-6 text-gray-900"
+          >
+            {"Carregamento Finalizado"}
+          </label>
+          <div class="grid mt-2">
+            <input
+              class="block
+         w-20 rounded-md border-0 
+        py-1.5 text-gray-900 shadow-sm
+         ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+         focus:ring-2 focus:ring-inset focus:ring-indigo-600
+          sm:text-sm sm:leading-6 text-center"
+              value={props.finalizarCarregamento}
+            />
+          </div>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 
+           text-sm font-semibold text-white 
+           shadow-sm hover:bg-indigo-500
+            focus-visible:outline focus-visible:outline-2
+             focus-visible:outline-offset-2
+              focus-visible:outline-indigo-600"
+            onClick={() => acaoFimCarregamento()}
+          >consolidar</button>
+
+        </div>
+
+        <div>
+          <label
+            for="first-name"
+            class="block text-sm font-medium leading-6 text-gray-900"
+          >
+            {"Chegada na loja"}
+          </label>
+          <div class="grid mt-2">
+            <input
+              class="block
+         w-20 rounded-md border-0 
+        py-1.5 text-gray-900 shadow-sm
+         ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+         focus:ring-2 focus:ring-inset focus:ring-indigo-600
+          sm:text-sm sm:leading-6 text-center"
+              value={props.chegadaLoja}
+            />
+          </div>
+          <button
+            type="button"
+            className="rounded-md bg-indigo-600 
+           text-sm font-semibold text-white 
+           shadow-sm hover:bg-indigo-500
+            focus-visible:outline focus-visible:outline-2
+             focus-visible:outline-offset-2
+              focus-visible:outline-indigo-600"
+            onClick={() => acaoCahegadaLoja()}
+          >consolidar</button>
+
+        </div>
+
+        <div>
+          <label
+            for="first-name"
+            class="block text-sm font-medium leading-6 text-gray-900"
+          >
+            {"Saida loja"}
+          </label>
+          <div class="grid mt-2">
+            <input
+              class="block
+         w-20 rounded-md border-0 
+        py-1.5 text-gray-900 shadow-sm
+         ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+         focus:ring-2 focus:ring-inset focus:ring-indigo-600
+          sm:text-sm sm:leading-6 text-center"
+              value={props.saidaLoja}
+            />
+          </div>
+          <button
+            type="button"
+            className="rounded-md bg-indigo-600 
+           text-sm font-semibold text-white 
+           shadow-sm hover:bg-indigo-500
+            focus-visible:outline focus-visible:outline-2
+             focus-visible:outline-offset-2
+              focus-visible:outline-indigo-600"
+            onClick={() => acaoSaidaLoja()}
+          >consolidar</button>
+
+        </div>
+
+        <div>
+          <label
+            for="first-name"
+            class="block text-sm font-medium leading-6 text-gray-900"
+          >
+            {"Chegada CD"}
+          </label>
+          <div class="grid mt-2">
+            <input
+              class="block
+         w-20 rounded-md border-0 
+        py-1.5 text-gray-900 shadow-sm
+         ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+         focus:ring-2 focus:ring-inset focus:ring-indigo-600
+          sm:text-sm sm:leading-6 text-center"
+              value={props.ChegadaCD}
+            />
+          </div>
+          <button
+            type="button"
+            className="rounded-md bg-indigo-600 
+           text-sm font-semibold text-white 
+           shadow-sm hover:bg-indigo-500
+            focus-visible:outline focus-visible:outline-2
+             focus-visible:outline-offset-2
+              focus-visible:outline-indigo-600"
+            onClick={() => acaoChegadaCD()}
+          >consolidar</button>
+
+        </div>
+
+
+
+      </div>
+    </div>
+  )
+
+}
---
+++ src/pages/ConsultarViagem/index.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { InputConsulta } from "../../components/Inputs/CampoHorario";
+import { CampoInputValueTexto } from "../../components/Inputs/CampoParaMostrarDados";
+import Status from "../../components/SinalizadorDeStatus";
+import TabelaDoConsulta from "../../components/TabelaAdicionarLojaEntrega/TabelaMostrado";
+import { bucsarViagemPorId } from "../../service";
+
+export default function ConsultaViagem() {
+
+
+  const [initCarregamento, setInitCarregamento] = useState()
+  const [finCarregamento, setFinCarregamento] = useState()
+  const [chegadaNaLoja, setChegadaNaLoja] = useState()
+  const [saidadaNaLoja, setSaidadaNaLoja] = useState()
+  const [chegadaNoCd, setChegadaNoCd] = useState()
+  const [programado, setProgramado] = useState() 
+  
+  const [motorista, setMotorista] = useState()
+  const [veiculo, setVeiculo] = useState()
+  const [tipo, setTipoVeiculo] = useState()
+  const [origem, setOrigem] = useState()
+  
+  const {id} = useParams();
+  
+  useEffect(() => {
+    bucsarViagemPorId(id)
+    .then((resp) => {
+      setOrigem(resp.data.origem.nome)
+      setTipoVeiculo(resp.data.veiculo.tipoDoVeiculo)
+      setVeiculo(resp.data.veiculo.placa)
+      setMotorista(resp.data.motorista.nome)
+
+      
+      setProgramado(resp.data.horaDoRegistro)
+      setInitCarregamento(resp.data.horarios.inicioDescarregamento);
+      setFinCarregamento(resp.data.horarios.fimDoCarregamento);
+      setChegadaNaLoja(resp.data.horarios.chegadaLoja);
+      setSaidadaNaLoja(resp.data.horarios.saidaLoja);
+      setChegadaNoCd(resp.data.horarios.chegadaCD);
+
+    }).catch(err => console.log(err))
+  })
+  
+  
+  
+  const [total, setTotal] = useState()
+
+  function totalPallets(e) {
+    setTotal(e)
+    console.log(total)
+  }
+
+  
+  
+  return (
+    <>
+      <Status id={id} />
+      <div className="inline-block group p-6 border-2 cursor-pointer hover:bg-gray-50 hover:border-blue-100">
+        <label>Viagem Nº: {id}</label>
+        <div className="grid grid-cols-4 ">
+          <CampoInputValueTexto label={"Motorista"} conteudo={motorista}/>
+          <CampoInputValueTexto label={"Placa"} conteudo={veiculo}/>
+          <CampoInputValueTexto label={"Veiculo"} conteudo={tipo}/>
+          <CampoInputValueTexto label={"Origem"} conteudo={origem}/>
+        </div>
+      </div>
+      <div className="inline-block group p-6 border-2 cursor-pointer hover:bg-gray-50 hover:border-blue-100 w-full">
+        <label>Entrega:</label>
+        <TabelaDoConsulta id={id} pegarTotalPallet={totalPallets}/>
+      </div>
+
+            <InputConsulta 
+              programado={programado}
+              iniciarCarregamento={initCarregamento}
+              finalizarCarregamento={finCarregamento}
+              chegadaLoja={chegadaNaLoja}
+              saidaLoja={saidadaNaLoja}
+              ChegadaCD={chegadaNoCd}
+              id={id}
+            />
+
+    </>
+  );
+}
