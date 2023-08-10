c65e37dd05963793c866e09f1439dd9267194200
daniprimo <daniellopes0208@hotmail.com>
2023-08-10 10:52:48 -0300

    Ajustando divergencia da linha 152

--- src/components/Inputs/CampoHorario/index.jsx
+++ src/components/Inputs/CampoHorario/index.jsx
@@ -133,6 +133,41 @@ export function InputConsulta(props) {
 
         </div>
 
+
+        <div>
+          <label
+            for="first-name"
+            class="block text-sm font-medium leading-6 text-gray-900"
+          >
+            {"Saida de loja"}
+          </label>
+          <div class="grid mt-2">
+            <input
+              class="block
+         w-20 rounded-md border-0 
+        py-1.5 text-gray-900 shadow-sm
+         ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+         focus:ring-2 focus:ring-inset focus:ring-indigo-600
+          sm:text-sm sm:leading-6 text-center"
+              value={props.saidaLoja}
+            />
+          </div>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 
+           text-sm font-semibold text-white 
+           shadow-sm hover:bg-indigo-500
+            focus-visible:outline focus-visible:outline-2
+             focus-visible:outline-offset-2
+              focus-visible:outline-indigo-600"
+            onClick={() => acaoSaidaLoja()}
+          >consolidar</button>
+
+        </div>
+
+
+
+
         <div>
           <label
             for="first-name"
