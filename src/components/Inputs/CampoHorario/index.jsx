27be6decdcc8a7ddb92b3451e3e91fc21a04bd46
daniprimo <daniellopes0208@hotmail.com>
2023-08-10 10:43:04 -0300

    Ajustando saida loja

--- src/components/Inputs/CampoHorario/index.jsx
+++ src/components/Inputs/CampoHorario/index.jsx
@@ -149,7 +149,7 @@ export function InputConsulta(props) {
          ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
          focus:ring-2 focus:ring-inset focus:ring-indigo-600
           sm:text-sm sm:leading-6 text-center"
-              value={props.saidaDoCD}
+              value={props.saidaLoja}
             />
           </div>
           <button
